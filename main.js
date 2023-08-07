448e261e3fc392c32c2c86ff8364ab2a71dc2cc4
kgcitizen <bekturboobekov@gmail.com>
2023-08-07 17:17:42 +0600

    Add button to control grid size and random-color trace

--- main.js
+++ main.js
@@ -1,18 +1,46 @@
 const container = document.querySelector('.container');
+const sizeBtn = document.getElementById('sizeBtn');
+createGrid(16);
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
+sizeBtn.addEventListener('click', (e) => {
+  const gridLength = +window.prompt('Enter the size (1-100)');
+  if (!Number.isInteger(+gridLength)) {
+    alert('Enter a whole number');
+    return;
+  } else if (gridLength < 1 || gridLength > 100) {
+    alert('Enter a whole number between 1 and 100');
+    return;
+  }
+  container.innerHTML = '';
+  createGrid(gridLength);
+
+  for (let item of container.children) {
+    item.style.padding = `calc(100% / ${gridLength} / 2)`;
+  }
+});
+
+function createGrid(length) {
+  const totalDivs = length * length;
+
+  for (let i = 0; i < totalDivs; i++) {
     const divBox = document.createElement('div');
     divBox.className = 'box';
     container.appendChild(divBox);
   }
-}
+  for (const item of container.children) {
+    item.addEventListener('mouseenter', leaveTrace);
 
-for (const item of container.children) {
-  item.addEventListener('mouseenter', leaveTrace);
+    function leaveTrace(e) {
+      e.target.style.background = `rgb(${getRandomColor()})`;
+    }
+  }
 
-  function leaveTrace(e) {
-    e.target.style.filter = 'brightness(140%)';
-    e.target.style.border = '1px solid transparent';
+  function getRandomColor() {
+    let r = Math.round(256 * Math.random());
+    let g = Math.round(256 * Math.random());
+    let b = Math.round(256 * Math.random());
+
+    console.log(r);
+    return `${r}, ${g}, ${b}`;
   }
 }
